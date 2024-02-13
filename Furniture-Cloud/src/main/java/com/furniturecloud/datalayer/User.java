1322363d1d1ed6e86eeaf76e2deb482a205d9893
vkalahas <vkalahas@ucsd.edu>
2024-02-12 16:52:35 -0800

    added cart stuff

--- Furniture-Cloud/src/main/java/com/furniturecloud/datalayer/User.java
+++ Furniture-Cloud/src/main/java/com/furniturecloud/datalayer/User.java
@@ -11,12 +11,54 @@ public class User {
 	String email;
 	String firstName;
 	String lastName;
-	List<Address> address;
-	List<Product> shoppingCart;
+	// change to list later to accommodate more addresses
+	Address address;
+	// List<Product> shoppingCart;
+	Cart mainCart;
+	Cart wishList;
+	
+	public User() {
+		
+		
+	}
+	
+	public User(String firstName, String lastName, String email, Address address) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.address = address;
+		this.mainCart = new Cart();
+		this.wishList = new Cart();
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 	
 //	int getQuantity(Product product) {		
 //		return shoppingCart.stream().reduce(0, (count, item) -> 
 //			(item.SKU == product.SKU) ? count+1 : count);
 //	}
 
+	
 }
