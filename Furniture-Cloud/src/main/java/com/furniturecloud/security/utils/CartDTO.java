a23e31842cbfa8e780526a4f29004f74e173fb08
Uday Raghavan <udayragh@gmail.com>
2024-03-06 11:41:16 -0600

    cartdto

---
+++ Furniture-Cloud/src/main/java/com/furniturecloud/security/utils/CartDTO.java
@@ -0,0 +1,12 @@
+package com.furniturecloud.security.utils;
+
+public class CartDTO {
+	String Cart;
+	String unavaiable;
+	public CartDTO(String cart, String unavaiable) {
+		super();
+		Cart = cart;
+		this.unavaiable = unavaiable;
+	}
+	
+}
